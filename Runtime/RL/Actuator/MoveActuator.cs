8dc58c4eb909fa59d86fa6752367ad03156a22d5
Gilzamir Gomes <54516662+gilzamir18@users.noreply.github.com>
2023-04-27 21:07:29 -0300

    Update MoveActuator.cs

--- Runtime/RL/Actuator/MoveActuator.cs
+++ Runtime/RL/Actuator/MoveActuator.cs
@@ -30,50 +30,46 @@ namespace ai4u {
 
         public override void Act()
         {
-            if (agent != null && !agent.Done)
+            Rigidbody rBody = agent.GetComponent<Rigidbody>();
+            if (rBody != null && agent != null && !agent.Done)
             {
                 float[] action = agent.GetActionArgAsFloatArray();
                 move = action[0];
                 turn = action[1];
                 jump = action[2];
                 jumpForward = action[3];
-
-                Rigidbody rBody = agent.GetComponent<Rigidbody>();
                 Transform reference = agent.gameObject.transform;
-                if (rBody != null)
+                if (Mathf.Abs(rBody.velocity.y) > 0.001)
                 {
-                    if (Mathf.Abs(rBody.velocity.y) > 0.001)
+                    onGround = false;
+                }
+                else
+                {
+                    onGround = true;
+                }
+                if (onGround)
+                {
+                    if (Mathf.Abs(turn) < 0.01f)
                     {
-                        onGround = false;
+                        turn = 0;
                     }
-                    else
-                    {
-                        onGround = true;
-                    }
-                    if (onGround)
-                    {
-                        if (Mathf.Abs(turn) < 0.01f)
-                        {
-                            turn = 0;
-                        }
 
-                        //Quaternion deltaRotation = Quaternion.Euler(reference.up * turn * turnAmount);
-                        //rBody.MoveRotation(rBody.rotation * deltaRotation);
-                        
-                        rBody.angularVelocity = Vector3.zero;
-                        rBody.AddTorque(reference.up * turn * turnAmount);
+                    //Quaternion deltaRotation = Quaternion.Euler(reference.up * turn * turnAmount);
+                    //rBody.MoveRotation(rBody.rotation * deltaRotation);
+                    
+                    rBody.angularVelocity = Vector3.zero;
+                    rBody.AddTorque(reference.up * turn * turnAmount);
 
-                        rBody.AddForce(
-                                        (jump * jumpPower * reference.up +
-                                        move * moveAmount * reference.forward + 
-                                        (
-                                            jumpPower * jumpForward * reference.up + 
-                                            jumpForward * jumpForwardPower * 
-                                            reference.forward )   
-                                        )
+                    rBody.AddForce(
+                                    (jump * jumpPower * reference.up +
+                                    move * moveAmount * reference.forward + 
+                                    (
+                                        jumpPower * jumpForward * reference.up + 
+                                        jumpForward * jumpForwardPower * 
+                                        reference.forward )   
+                                    )
 
-                                      );
-                    }
+                                    );
                 }
                 move = 0;
                 turn = 0;
